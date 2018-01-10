ce21ab16e59f7da5c5826f56b712682cd95da7bd
Ricardo Rodrigues <ricrobert.rr@gmail.com>
2018-01-10 20:13:45 -0200

    REST testado

--- src/main/java/com/ricardorodrigues/cursomc/CursomcApplication.java
+++ src/main/java/com/ricardorodrigues/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ricardorodrigues.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
