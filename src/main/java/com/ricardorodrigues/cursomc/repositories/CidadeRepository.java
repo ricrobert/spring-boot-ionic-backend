d245f37b6a489f1d7985d62479235d0a92e46c63
Ricardo Rodrigues <ricrobert.rr@gmail.com>
2018-01-15 18:27:22 -0200

    Estado e cidade

---
+++ src/main/java/com/ricardorodrigues/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.ricardorodrigues.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ricardorodrigues.cursomc.domain.Cidade;
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
